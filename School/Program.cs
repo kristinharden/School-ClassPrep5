129c3216a3ed81f71b88987c9c547e363c0940fa
KristinHarden <KristinHarden@gmail.com>
2018-02-22 11:05:00 -0500

    Tried Class prep 5. No clue how to include a list or dictionary or how to use Student class in Course class.

--- School/Program.cs
+++ School/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace School
 {
-    class Program
+
+    public class Student
     {
-        static void Main(string[] args)
+
+        private static int nextStudentId = 1;
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public int NumberOfCredits { get; set; }
+        public double Gpa { get; set; }
+
+        public Student(string name, int numberOfCredits,
+                        double gpa)
         {
-            Console.WriteLine("Hello World!");
+            StudentId = nextStudentId++;
+            Name = name;
+            NumberOfCredits = numberOfCredits;
+            Gpa = gpa;
         }
     }
+
+    public class Course
+    {
+        public string Name { get; set; }
+        public int CourseId { get; set; }
+        public string ProfLastName { get; set; }
+        public int NumberOfCredits { get; set; }
+        public List<int> EnrolledStudents = new List<int>
+
+        public Course(string name, int courseid, string profname, int credits, )
+
+
+    }
 }
+
+
+
